e380c918131d60d59116d64c4c243165d27ef13f
Therese <therese.holen22@gmail.com>
2023-03-01 15:02:11 +0100

    Code from todays lesson

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,27 @@
 var express = require('express');
 var router = express.Router();
 
+let users = [
+  {id: 1, name: "Anna", password: "minkattHugo"},
+  {id: 1, name: "Bengt", password: "Seglaelivet"},
+  {id: 1, name: "Cissi", password: "Godchoklad"},
+  {id: 1, name: "Daniel", password: "MFFrockar"},
+  {id: 1, name: "Eric", password: "Jagvillhahelg"}
+]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Users hello');
+  res.json(users);
+});
+
+router.post('/', function(req, res, next) {
+
+  let newUser = req.body;
+  newUser.id = users.length + 1;
+  users.push(newUser);
+  console.log(newUser);
+
+  res.json(users);
 });
 
 module.exports = router;
