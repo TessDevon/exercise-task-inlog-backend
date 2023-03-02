dd51bd6eea5bdd83bb3844d2a04ff6c251c1332c
Therese <therese.holen22@gmail.com>
2023-03-02 12:57:27 +0100

    Project done

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const cors = require('cors')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+//var usersiteRouter = require('./routes/usersite');
 
 var app = express();
 
@@ -19,5 +20,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+//app.use('/usersite', usersiteRouter);
 
 module.exports = app;
--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 let users = [
-  {id: 1, name: "Anna", password: "minkattHugo"},
-  {id: 1, name: "Bengt", password: "Seglaelivet"},
-  {id: 1, name: "Cissi", password: "Godchoklad"},
-  {id: 1, name: "Daniel", password: "MFFrockar"},
-  {id: 1, name: "Eric", password: "Jagvillhahelg"}
+  {id: 86574, name: "Anna", password: "minkattHugo"},
+  {id: 74589, name: "Bengt", password: "Seglaelivet"},
+  {id: 25896, name: "Cissi", password: "Godchoklad"},
+  {id: 15874, name: "Daniel", password: "MFFrockar"},
+  {id: 74845, name: "Eric", password: "Jagvillhahelg"}
 ]
 
 /* GET users listing. */
@@ -16,12 +16,38 @@ router.get('/', function(req, res, next) {
 
 router.post('/', function(req, res, next) {
 
+  let loginData = req.body;
+
+  for (let i = 0; i < users.length; i++) {
+    if (req.body.name == users[i].name) {
+      if (req.body.password == users[i].password){
+        //let okId = users[i].id
+
+        //res.writeHead(200, {"Content-Type": "text/plain"});
+        res.json({id: users[i].id});
+        console.log(users[i].id);
+        return
+      }
+    }
+  }
+
+  res.writeHead(404, {"Content-Type": "text/plain"});
+  res.end("Your user is not found!");
+
+  //console.log(loginData);
+
+//  res.json(users);
+});
+
+/*
+router.post('/', function(req, res, next) {
+
   let newUser = req.body;
   newUser.id = users.length + 1;
   users.push(newUser);
   console.log(newUser);
 
   res.json(users);
-});
+});*/
 
 module.exports = router;
