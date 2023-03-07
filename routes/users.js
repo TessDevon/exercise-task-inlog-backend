35aa583fc0233b128ce95fd80f7de1f100290c69
Therese <therese.holen22@gmail.com>
2023-03-07 10:03:20 +0100

    Second week Cryptotask

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var CryptoJS = require("crypto-js");
 
 let users = [
   {id: 86574, name: "Anna", password: "minkattHugo"},
@@ -9,23 +10,42 @@ let users = [
   {id: 74845, name: "Eric", password: "Jagvillhahelg"}
 ]
 
-/* GET users listing. */
+/* GET users listing. 
 router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+
+  var userPass = "MittFantastiskaLösenord";
+  console.log(userPass);
+
+  var krypteratPass = CryptoJS.AES.encrypt(userPass, "Salt Nyckel").toString();
+  console.log(krypteratPass);
+
+  var originalPass = CryptoJS.AES.decrypt(krypteratPass, "Salt Nyckel").toString(CryptoJS.enc.Utf8);
+  console.log(originalPass);
+
+});*/
+
+router.get('/', function(req, res, next) {
+
   res.json(users);
 });
 
-router.post('/', function(req, res, next) {
+
+/*--------------- Kod för att hantera inloggning -----------------------------------------------*/
+
+router.post('/login', function(req, res, next) {
 
   let loginData = req.body;
 
   for (let i = 0; i < users.length; i++) {
     if (req.body.name == users[i].name) {
-      if (req.body.password == users[i].password){
-        //let okId = users[i].id
+      var originalPass = CryptoJS.AES.decrypt(users[i].password, "Salt Nyckel").toString(CryptoJS.enc.Utf8);
 
-        //res.writeHead(200, {"Content-Type": "text/plain"});
+      console.log(users[i].password);
+      if (loginData.password == originalPass){
+
         res.json({id: users[i].id});
-        console.log(users[i].id);
+      //  console.log(users[i].id);
         return
       }
     }
@@ -36,18 +56,23 @@ router.post('/', function(req, res, next) {
 
   //console.log(loginData);
 
-//  res.json(users);
 });
 
-/*
+
+/*--------------------------------Kod för att hantera tillägg av ny användare-------------------------------*/
+
 router.post('/', function(req, res, next) {
 
+  var krypteratPassNew = CryptoJS.AES.encrypt(req.body.password, "Salt Nyckel").toString();
+  console.log(krypteratPassNew);
+
   let newUser = req.body;
+  newUser.password = krypteratPassNew;
   newUser.id = users.length + 1;
   users.push(newUser);
   console.log(newUser);
 
   res.json(users);
-});*/
+});
 
 module.exports = router;
